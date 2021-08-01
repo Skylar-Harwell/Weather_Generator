79d7ac1bab7a1830c941b29e79ce64afe6d5a251
Skylar Harwell <swharwell@gmail.com>
2021-08-01 19:23:17 -0400

    Updated Coding and added buttons to the previous search.

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,9 +10,19 @@ var tempEl = document.querySelector('#temp');
 var humidityEl = document.querySelector('#humidity');
 var windEl = document.querySelector('#wind');
 var descEl = document.querySelector('#desc');
+var currIconEl = document.querySelector('#currIcon');
 
 
 button.addEventListener('click',function() {
+    getCurrentWeather();
+    saveSearch();
+}); 
+
+reset.addEventListener('click',function() {
+    getFutureWeather();
+});
+
+var getCurrentWeather = function () {
 
 var weatherUrl = 'https://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&units=imperial&appid=6ba1d98ca929f44f2e4eb95a22b0557a';
 
@@ -22,20 +32,35 @@ fetch(weatherUrl)
         response.json().then(function (data) {
             console.log(data);
             var cityValue = 'City: ' + data.name;
-            // var dateValue = (data.dt).format('MM/DD/YYYY');
             var humidityValue = 'Humidity: ' + data.main.humidity + ' %';
             var windValue = 'Wind Speed: ' + data.wind.speed + ' MPH';
-            var tempValue = 'Temp: ' + data.main.temp;
-            var descValue = 'Description: ' + data['weather'][0]['description'];
+            var tempValue = 'Temp: ' + data.main.temp + '°F';
+            var descValue = 'Description: ' + data.weather[0].description;
 
+            var a = new Date(data.dt * 1000);
+            var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+            var year = a.getFullYear();
+            var month = months[a.getMonth()];
+            var date = a.getDate();
+            var time = month + ' ' + date + ' ' + year;
+
+            var iconValue = data.weather[0].icon;
+            var iconUrl = "http://openweathermap.org/img/wn/" + iconValue + ".png";
+
+            currIconEl.classList.remove("hide");
+            currIconEl.src = iconUrl;
+            resultsContainer.appendChild(currIconEl);
 
             cityEl.innerHTML = cityValue;
-            // dateEl.innerHTML = dateValue;
+            dateEl.innerHTML = time;
             tempEl.innerHTML = tempValue;
             humidityEl.innerHTML = humidityValue;
             windEl.innerHTML = windValue;
             descEl.innerHTML = descValue;
+
+            localStorage.pastSearch = inputValue.value;
     });
+
     } else {
         alert('Error: ' + response.statusText);
     }
@@ -43,9 +68,9 @@ fetch(weatherUrl)
     .catch(function (error) {
         alert('Unable to connect to Weather');
     });
-});
+};
 
-var getCurrentWeather = function () {
+var getFutureWeather = function () {
     var forecastUrl = 'https://api.openweathermap.org/data/2.5/forecast?q='+inputValue.value+'&units=imperial&appid=6ba1d98ca929f44f2e4eb95a22b0557a';
 
     fetch(forecastUrl)
@@ -63,6 +88,14 @@ var getCurrentWeather = function () {
     });
 };
 
-reset.addEventListener('click',function() {
-    getCurrentWeather();
-});
+var saveSearch = function () {
+    var button = document.createElement('button');
+    button.innerText = inputValue.value;
+    button.classList.add('btn');
+    pastSearches.appendChild(button);
+    button.addEventListener('click', getCurrentWeather)
+};
+
+function loadSearch() {
+    $('#btn').val(localStorage.pastSearch);
+}
